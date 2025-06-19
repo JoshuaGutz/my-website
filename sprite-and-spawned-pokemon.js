b64372aa84e147b3a7cc540abdc1d3f7de30c6a4
Joshua Gutz <jgutz87@gmail.com>
2025-06-19 04:00:32 -0500

    Updated sprite-and-spawned-pokemon.js debugged issue of previous spawn showing on first load

--- sprite-and-spawned-pokemon.js
+++ sprite-and-spawned-pokemon.js
@@ -27,33 +27,45 @@ function display_questionmark() {
     spawnedPokemonIdElement.textContent = `No current spawn`;
 }
 
-var last_pokedex_id = 0;
+let last_pokedex_id = 0;
+let pokedex_id;
+let next_spawn;
+let order;
+// console.log(`DEBUG1 last_pokedex_id=${last_pokedex_id ?? 'na'} pokedex_id=${pokedex_id ?? 'na'} next_spawn=${next_spawn ?? 'na'}`);
 
 function update_image() {
-    // fetch image
+    // console.log(`DEBUG2 last_pokedex_id=${last_pokedex_id ?? 'na'} pokedex_id=${pokedex_id ?? 'na'} next_spawn=${next_spawn ?? 'na'}`);
     fetch(backend_url + 'info/events/last_spawn/').then(function (response) {
         return response.json();
     }).then(function (data) {
-        var next_spawn = data.next_spawn
-        var pokedex_id = data.pokedex_id
-        var order = data.order
+        // console.log("Raw JSON fetched:", data);
+        next_spawn = data.next_spawn
+        pokedex_id = data.pokedex_id
+        order = data.order
+        // console.log(`DEBUG3 last_pokedex_id=${last_pokedex_id ?? 'na'} pokedex_id=${pokedex_id ?? 'na'} next_spawn=${next_spawn ?? 'na'}`);
 
         if (next_spawn > (13 * 60) + 30) {
-            if (last_pokedex_id === pokedex_id) {
+            // console.log(`DEBUG4 last_pokedex_id=${last_pokedex_id ?? 'na'} pokedex_id=${pokedex_id ?? 'na'} next_spawn=${next_spawn ?? 'na'}`);
+            if ((next_spawn == 900 && last_pokedex_id == 0) || last_pokedex_id === pokedex_id) {
+                // console.log(`DEBUG5 last_pokedex_id=${last_pokedex_id ?? 'na'} pokedex_id=${pokedex_id ?? 'na'} next_spawn=${next_spawn ?? 'na'}`);
                 setTimeout(function () {
                     update_image();
                 }, 3000);
+                // console.log(`DEBUG6 last_pokedex_id=${last_pokedex_id ?? 'na'} pokedex_id=${pokedex_id ?? 'na'} next_spawn=${next_spawn ?? 'na'}`);
                 return;
             } else {
+                // console.log(`DEBUG7 last_pokedex_id=${last_pokedex_id ?? 'na'} pokedex_id=${pokedex_id ?? 'na'} next_spawn=${next_spawn ?? 'na'}`);
                 last_pokedex_id = pokedex_id
+                // console.log(`DEBUG8 last_pokedex_id=${last_pokedex_id ?? 'na'} pokedex_id=${pokedex_id ?? 'na'} next_spawn=${next_spawn ?? 'na'}`);
             }
 
             // update and return if the pokedex id didn't update
+            // console.log(`DEBUG9 last_pokedex_id=${last_pokedex_id ?? 'na'} pokedex_id=${pokedex_id ?? 'na'} next_spawn=${next_spawn ?? 'na'}`);
             sprite.src = image_url + "static/pokedex/sprites/front/" + pokedex_id + ".gif";
             let tier = getColumnByPokedexId(pokedex_id, 2);
             let name = getColumnByPokedexId(pokedex_id, 5);
             let gen = getColumnByPokedexId(pokedex_id, 4);
-            spawnedPokemonIdElement.textContent = `${name} Tier:${tier} Gen:${gen} ID:${pokedex_id}`; // Furfrou-heart Tier:B Gen:6 ID:10344
+            spawnedPokemonIdElement.textContent = `${name} Tier:${tier} Gen:${gen} ID:${pokedex_id}`; // ex Furfrou-heart Tier:B Gen:6 ID:10344
 
             // bild nach den 90 sekunden verstecken
             var hide_picture_seconds = next_spawn - 810
@@ -63,7 +75,6 @@ function update_image() {
 
             // refresh image in 810 seconds
             setTimeout(function () {
-                // bild auf fragezeichen
                 display_questionmark();
                 // refresh image when new pokemon spawns
                 setTimeout(function () {
@@ -71,27 +82,21 @@ function update_image() {
                 }, (next_spawn - hide_picture_seconds) * 1000);
             }, hide_picture_seconds * 1000);
 
-            // too late
         } else {
-            // bild auf fragezeichen
             display_questionmark();
-
-
-            // warte solange wie der nächste spawn braucht und update dann das image
             setTimeout(function () {
                 update_image();
             }, next_spawn * 1000);
         }
     }).catch(function (error) {
-        // bild auf fragezeichen
         display_questionmark();
 
         // fetch again in 10 seconds if fail
         setTimeout(function () {
+            // console.log(`DEBUG10 last_pokedex_id=${last_pokedex_id ?? 'na'} pokedex_id=${pokedex_id ?? 'na'} next_spawn=${next_spawn ?? 'na'}`);
             update_image();
         }, 10000);
     });
-
 }
 
 window.addEventListener('load', async () => {
